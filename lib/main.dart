f715d8a64255024f1894ed6020c97f490ec01e3f
Philip Paolo De Castro <philip.paolo.de.castro@gmail.com>
2024-04-15 09:51:08 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('assets/images/poor.png'),
+            ),
+            const Text(
+              'Pipaw De Castro',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'SOFTWARE ENGINEER',
+              style: TextStyle(
+                  fontFamily: 'SourceSans',
+                  fontSize: 20,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal[100],
+              ),
+            ),
+            Card(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: const Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+63 907-933-9116',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: const Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'philip.paolo.de.castro@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                  ),
+                ),
+              ),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
